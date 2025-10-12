fced0b667c639c4f19e824a71b53e2bbd75ada7f
parkjihyoun <jihyoun2002@knu.ac.kr>
2025-10-12 11:13:32 +0900

    feat: User 엔티티 및 BaseEntity 생성

---
+++ src/main/java/com/studiz/domain/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.studiz.domain.user.entity;
+
+import com.studiz.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Builder
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
+
